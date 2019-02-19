233f68bed825bae181c63fe899f925e4f5fb8ca7
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-02-19 04:36:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+  if num%2 != 0
+    if num%3 != 0
+      if num%5 != 0
+        if num%7 != 0
+          if num%11 !=0
+            return true
+          end
+        end
+      end
+    end
+    return false
+  end
+end
